62752db062787fefc32bc36a6f994316136ba58f
Luis <cifuentes_estrada@hotmail.com>
2019-07-12 00:06:58 -0600

    Library #000001

---
+++ xbixolon/src/main/java/com/almo/xbixolon/ViewGenerator.java
@@ -0,0 +1,15 @@
+package com.almo.xbixolon;
+
+import android.util.Log;
+
+public class ViewGenerator {
+
+    private static final String TAG = "XBIXOLON";
+
+    public static String test(String test) {
+        Log.d(TAG, "test: " + test);
+        return test;
+    }
+
+
+}
